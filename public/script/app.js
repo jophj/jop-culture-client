391231587556a2fee6a4b602872b2fce416785e7
Giovanni Pinto <jop.mezzana@gmail.com>
2015-03-08 16:37:24 +0100

    Added jc-item directive

--- public/script/app.js
+++ public/script/app.js
@@ -9,13 +9,35 @@ app.factory('Music', function($http){
   }
 });
 
+app.directive('myCustomer', function() {
+  return {
+    template: 'Name: {{customer.name}} Address: {{customer.address}}'
+  };
+});
+
+/**
+ An item element. Could be an album or a movie etc..
+*/
+app.directive('jcItem', function(){
+  return{
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      item: '=data'
+    },
+    templateUrl: 'jc-item.html',
+    controller: function($scope){
+      console.log($scope.album);
+    }
+  };
+});
+
 app.controller('AppCtrl', ['$scope', 'Music', function($scope, Music){
 
   $scope.albums = [];
 
   Music.saved().then(function(response){
     $scope.albums = response.data.albums;
-    console.log($scope.albums);
   });
 
 }]);
