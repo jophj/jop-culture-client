8916f15a3d4aaefa1c0bc5a4376a67ac797244a0
Giovanni Pinto <jop.mezzana@gmail.com>
2015-04-19 19:15:54 +0200

    WIP: scrolling and loading

--- public/script/app.js
+++ public/script/app.js
@@ -1,40 +1,77 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
-
+/**
+  Section names are:
+    music
+    movies
+    books
+  YOLO, no tiem for constants
+*/
 app.factory('Music', function($http){
   var MUSIC_API = 'https://jop-culture.herokuapp.com/music/';
   //var MUSIC_API = 'http://localhost:3666/music/';
   return {
-    saved: function(){
-      return $http.get(MUSIC_API+ 'saved');
+    saved: function(offset, limit){
+      return $http.get(MUSIC_API+ 'saved', {params: {"limit": limit, "offset": offset}});
     }
   }
 });
 
-app.directive('myCustomer', function() {
-  return {
-    template: 'Name: {{customer.name}} Address: {{customer.address}}'
-  };
+app.factory('DataProvider', function(Music){
+
+  /* Here's the configurations of data providers */
+  providers = {
+    "music": {
+      "saved": Music.saved
+    }
+  }
+
+  selector = function(source){
+    return providers[source];
+  }
+  return selector;
 });
 
-/**
- An item element. Could be an album or a movie etc..
-*/
-app.directive('jcGrid', function(){
+app.controller('AppCtrl', [
+  '$scope', 'Music', '$window',
+  function($scope, Music, $window){
+    
+    $scope.section = 'music';
+
+    hash = $window.location.hash;
+
+    var regexp = /^#\/([^\/]+)\/?.*$/;
+    var match = regexp.exec(hash);
+
+    if (match != null && match[1] != null){
+      $scope.section = match[1];
+    }
+  }
+]);
+
+app.controller('gridCtrl', [
+  '$scope', 'DataProvider', '$window',
+  function($scope, DataProvider, $window){
+
+    var limit = 50;      //loads no more than limit items every call
+    var offset = 0;
+    $scope.items = [];
+
+    DataProvider($scope.section).saved(0, limit).then(function(response){
+      $scope.items = response.data.albums;
+      offset = response.data.offset;
+    });
+
+    //TODO the watcher for $scope.section that loads data
+  }
+]);
+
+app.directive('scrollLoad', function(){
   return{
-    restrict: 'E',
-    transclude: true,
-    scope: {
-      items: '=items'
-    },
-    templateUrl: 'jc-grid.html',
-  };
+    restrict: 'A',
+    link: function(scope, element, attrs){
+      element.bind('scroll', function(){
+        console.log('h4x0rz');
+      });
+    }
+  }
 });
-
-app.controller('AppCtrl', ['$scope', 'Music', function($scope, Music){
-
-  $scope.items = [];
-
-  Music.saved().then(function(response){
-    $scope.items = response.data.albums;
-  });
-}]);
